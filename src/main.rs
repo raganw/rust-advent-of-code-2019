3c8764862105c98f8aa234e486b0e37c22c778b4
Ragan Webber <ragan@ragan.is>
2019-12-11 13:58:29 -0800

    Day 1 - part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let contents = fs::read_to_string(filename).expect("Unable to read file");
+    let lines: Vec<i32> = contents.lines().map(|s|  s.parse().unwrap()).collect();
+    let total_fuel = lines.iter().fold(0, |acc, &i| acc + calculate_fuel(i));
+
+    println!("Total fuel = {}", total_fuel)
+}
+
+fn calculate_fuel(mass: i32) -> i32 {
+    mass / 3 - 2
 }
